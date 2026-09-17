e7d00d19818e6dd3daa037ba3edf9625dc47e5bf
gabrielaPLMPRO <gabriela@plmpro.com.br>
2026-09-17 20:36:14 -0300

    test(RF08): automatiza consulta de conta existente

---
+++ src/negocio/BDD/RF08.java
@@ -0,0 +1,32 @@
+package negocio.BDD;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import negocio.ContaCorrente;
+import negocio.GerenciadoraContas;
+
+public class RF08 {
+
+    @Test
+    public void deveConsultarContaExistente() {
+
+        List<ContaCorrente> contas = new ArrayList<ContaCorrente>();
+
+        ContaCorrente conta = new ContaCorrente(1, 1000.0, true);
+
+        contas.add(conta);
+
+        GerenciadoraContas gerContas = new GerenciadoraContas(contas);
+
+        ContaCorrente contaEncontrada = gerContas.pesquisaConta(1);
+
+        assertNotNull(contaEncontrada);
+        assertEquals(1, contaEncontrada.getId());
+    }
+}
