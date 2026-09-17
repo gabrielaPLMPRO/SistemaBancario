0bf1bff0f5c3584470c65767618ca5ccf458e285
gabrielaPLMPRO <gabriela@plmpro.com.br>
2026-09-16 22:21:30 -0300

    test(RF09): adiciona RED para cadastro de conta

---
+++ src/TDD/RF09.java
@@ -0,0 +1,36 @@
+package TDD;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import negocio.Cliente;
+import negocio.ContaCorrente;
+import negocio.GerenciadoraClientes;
+import negocio.GerenciadoraContas;
+
+public class RF09 {
+
+    @Test
+    public void deveCadastrarContaParaClienteExistente() throws Exception {
+
+        List<Cliente> clientes = new ArrayList<>();
+
+        clientes.add(new Cliente(1, "Maria Silva", 30, "maria@email.com", 2, true));
+
+        GerenciadoraClientes gerClientes = new GerenciadoraClientes(clientes);
+
+        GerenciadoraContas gerContas = new GerenciadoraContas(new ArrayList<ContaCorrente>());
+
+        ContaCorrente novaConta = new ContaCorrente(2, 0, true);
+
+        assertTrue(gerContas.cadastraContaParaCliente(novaConta, 1, gerClientes));
+
+        assertEquals(novaConta, gerContas.pesquisaConta(2));
+    }
+
+}
