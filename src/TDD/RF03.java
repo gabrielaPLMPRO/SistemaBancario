512842c9d93f10880d095af21b4ad41cb98cca1a
gabrielaPLMPRO <gabriela@plmpro.com.br>
2026-09-16 22:00:32 -0300

    test(RF03): adiciona RED para cadastro de menor de idade

---
+++ src/TDD/RF03.java
@@ -0,0 +1,21 @@
+package TDD;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import negocio.Cliente;
+import negocio.GerenciadoraClientes;
+import negocio.IdadeNaoPermitidaException;
+
+public class RF03 {
+    
+    @Test(expected = IdadeNaoPermitidaException.class)
+    public void naoDeveCadastrarClienteMenorDeIdade() throws Exception {
+        GerenciadoraClientes gerClientes = new GerenciadoraClientes(new ArrayList<Cliente>());
+
+        Cliente clienteMenor = new Cliente(2, "Joao", 17, "joao@email.com", 2, true);
+
+        gerClientes.cadastraCliente(clienteMenor);
+    }
+}
